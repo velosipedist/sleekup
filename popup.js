fe052997942cd0da63c8ae3b461f67734ac808e5
Nikolai Konev <nick@konev.pro>
2023-11-20 19:47:36 +0800

    Fix data duplication

--- popup.js
+++ popup.js
@@ -1,17 +1,26 @@
 async function readItems () {
   const data = await chrome.storage.sync.get('scrummy_items');
-  const tpl = document.getElementById('record');
+  const taskTemplate = document.getElementById('record');
   const root = document.getElementById('scrum');
   root.childNodes.forEach((n) => root.removeChild(n));
 
   if (!data?.scrummy_items) {
     return;
   }
-  data.scrummy_items.forEach((item) => {
-    const record = tpl.content.cloneNode(true);
+
+  const dataDenormalized = data.scrummy_items.reduce((acc, next) => {
+    if (acc.some((d) => d.taskId === next.taskId)) {
+      return acc;
+    }
+    return [...acc, next];
+  }, []);
+
+  dataDenormalized.forEach((item) => {
+    const record = taskTemplate.content.cloneNode(true);
     record.querySelector('.id').textContent = item.taskId;
     record.querySelector('.id').href = item.url;
-    record.querySelector('.name').textContent = item.name;
+    record.querySelector('.name').textContent = item.name.trim();
+    record.querySelector('.name').attributes.title = item.name.trim();
     root.appendChild(record);
   });
 }
