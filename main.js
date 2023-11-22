4e2401bdd7435e4a89f3fb0c46f3788a98283769
Nikolai Konev <nick@konev.pro>
2023-11-22 11:08:42 +0800

    Update UI, show hours summary counter

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const observer = new MutationObserver(catchDom);
 
 observer.observe(document.body,
   { subtree: true, attributes: true, childList: true, characterData: false });
-const foo =observer.takeRecords();
 
 function catchDom (evt) {
   const itemsChecked = findAll('.cu-task-row.cu-task-row_selected');
@@ -11,16 +10,21 @@ function catchDom (evt) {
 
     itemsChecked.forEach((item, idx) => {
       const link = item.querySelector('a.cu-task-row-main__link');
+
       if (link) {
         const url = link.href;
         let taskId = '';
-        const taskIdMatch = url.match(/[a-z]+-\d+/i);
+        const taskIdMatch = url.match(/[A-Z]{3,5}-\d+/);
         if (taskIdMatch !== null) {
           taskId = taskIdMatch[0];
         }
         const name = link.querySelector(
           '.cu-task-row-main__link-text-inner')?.textContent ?? 'no name';
-        list.push({ name, url, taskId });
+
+
+        const timeCell = item.querySelector('[data-test="time-estimates-view__value"]');
+        const time = timeCell?.innerText;
+        list.push({ name, url, taskId, time });
       }
     });
 
--- popup.js
+++ popup.js
@@ -1,11 +1,24 @@
+window.onload = () => {
+  const copyBtn = document.getElementById('copy');
+  copyBtn.addEventListener('click', copyAllData);
+  readItems();
+};
+
 async function readItems () {
   const data = await chrome.storage.sync.get('scrummy_items');
   const taskTemplate = document.getElementById('record');
-  const root = document.getElementById('scrum');
-  root.childNodes.forEach((n) => root.removeChild(n));
+  const itemsRoot = document.querySelector('[data-items]');
+  itemsRoot.childNodes.forEach((n) => itemsRoot.removeChild(n));
+
+  const emptyListNode = document.querySelector('[data-no-items]');
+  const estimateNode = document.querySelector('[data-hours]');
 
   if (!data?.scrummy_items) {
+    hide(itemsRoot, estimateNode);
+    show(emptyListNode);
     return;
+  } else {
+    hide(emptyListNode);
   }
 
   const dataDenormalized = data.scrummy_items.reduce((acc, next) => {
@@ -15,20 +28,30 @@ async function readItems () {
     return [...acc, next];
   }, []);
 
+  let hoursTotal = 0;
   dataDenormalized.forEach((item) => {
     const record = taskTemplate.content.cloneNode(true);
     record.querySelector('.id').textContent = item.taskId;
     record.querySelector('.id').href = item.url;
     record.querySelector('.name').textContent = item.name.trim();
-    record.querySelector('.name').attributes.title = item.name.trim();
-    root.appendChild(record);
+    record.querySelector('.name').setAttribute('title', item.name.trim());
+    if (item.time) {
+      const hours = parseHours(item.time);
+      hoursTotal += hours;
+    }
+    itemsRoot.appendChild(record);
   });
+
+  show(itemsRoot);
+
+  if (hoursTotal > 0) {
+    estimateNode.innerText = `${hoursTotal} hours`;
+    show(estimateNode);
+  }
 }
 
-readItems();
-
 function copyAllData () {
-  const contents = document.getElementById('scrum').innerHTML;
+  const contents = document.querySelector('[data-items]').innerHTML;
 
   const clipboardItem = new ClipboardItem({
     'text/plain': new Blob([contents], { type: 'text/plain' }),
@@ -39,5 +62,28 @@ function copyAllData () {
     .catch(console.error);
 }
 
-const copyBtn = document.getElementById('copy');
-copyBtn.addEventListener('click', copyAllData);
+function show (...elements) {
+  for (var element of elements) {
+    element.classList.remove('invisible');
+    element.classList.remove('hidden');
+  }
+}
+
+function hide (...elements) {
+  for (var element of elements) {
+    element.classList.add('hidden');
+  }
+}
+
+function parseHours (input) {
+  let result = 0;
+  const hoursMatch = input.match(/(\d+)h/);
+  if (hoursMatch) {
+    result += parseInt(hoursMatch[1]);
+  }
+  const daysMatch = input.match(/(\d+)d/);
+  if (daysMatch) {
+    result += parseInt(daysMatch[1]) * 8;
+  }
+  return result;
+}
