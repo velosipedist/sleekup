c565f824c381f01383ab010c9738a66745b6da7d
Nikolai Konev <nick@konev.pro>
2024-01-11 11:46:15 +0800

    Optimize listening/writing rate (Fix #2)

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
-const observer = new MutationObserver(catchDom);
+const observer = new MutationObserver(catchCheckbox);
 
-observer.observe(document.body,
-  { subtree: true, attributes: true, childList: true, characterData: false });
+observer.observe(document.body, {
+    subtree: true,
+    attributes: true,
+    childList: false,
+    characterData: false,
+    attributeFilter: ['class'],
+  },
+);
 
-function catchDom (evt) {
+function catchCheckbox (mutations) {
+  const hasCheckMutations = mutations.some(
+    (m) => [...m.target.classList.values()]
+      .includes('cu-task-row-toggle'));
+  if (!hasCheckMutations) {
+    return;
+  }
+
   const itemsChecked = findAll('.cu-task-row.cu-task-row_selected');
   const list = [];
-  if (itemsChecked.length > 0) {
-    itemsChecked.forEach((item, idx) => {
-      const link = item.querySelector('a.cu-task-row-main__link');
+  itemsChecked.forEach((item, idx) => {
+    const link = item.querySelector('a.cu-task-row-main__link');
 
-      if (link) {
-        const url = link.href;
-        let taskId = '';
-        const taskIdMatch = url.match(/[A-Z]{3,5}-\d+/);
-        if (taskIdMatch !== null) {
-          taskId = taskIdMatch[0];
-        }
-        const name = link.querySelector(
-          '.cu-task-row-main__link-text-inner')?.textContent ?? 'no name';
-
-        const timeCell = item.querySelector(
-          '[data-test="time-estimates-view__value"]');
-        const time = timeCell?.innerText;
-        list.push({ name, url, taskId, time });
+    if (link) {
+      const url = link.href;
+      let taskId = '';
+      const taskIdMatch = url.match(/[A-Z]{3,5}-\d+/);
+      if (taskIdMatch !== null) {
+        taskId = taskIdMatch[0];
       }
-    });
-  }
+      const name = link.querySelector(
+        '.cu-task-row-main__link-text-inner')?.textContent ?? 'no name';
+
+      const timeCell = item.querySelector(
+        '[data-test="time-estimates-view__value"]');
+      const time = timeCell?.innerText;
+      list.push({ name, url, taskId, time });
+    }
+  });
 
   chrome.storage.sync.set({ scrummy_items: list });
 }
