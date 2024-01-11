84696e27d785c290e9fb8d1243e7489583733cdd
Nikolai Konev <nick@konev.pro>
2024-01-11 11:45:14 +0800

    Remove zipping, use crx build only. Optimize build

--- build.sh
+++ build.sh
@@ -1,3 +1,22 @@
 npx tailwindcss -i ./base.dev.css -o ./base.css
-rm build.zip
-zip -r build.zip . -x \node_modules\* \.*\* package.json yarn.lock \*.sh tailwind.config.js \*.md
+rm -Rf ./build
+mkdir -p ./build/icons/
+mkdir -p ./build/webfonts/
+ln ./icons/*.* ./build/icons/
+ln ./webfonts/*.* ./build/webfonts/
+ln ./base.css ./build/
+ln ./lib.js ./build/
+ln ./main.js ./build/
+ln ./popup.js ./build/
+ln ./*.html ./build/
+ln ./manifest.json ./build/
+#cp -Rf ./icons/*.* ./build/icons/
+#cp -Rf ./webfonts/*.* ./build/webfonts/
+#cp -Rf ./base.css ./build/
+#cp -Rf ./lib.js ./build/
+#cp -Rf ./main.js ./build/
+#cp -Rf ./popup.js ./build/
+#cp -Rf ./*.html ./build/
+#cp -Rf ./manifest.json ./build/
+#rm build.zip
+#zip -r build.zip . -x \node_modules\* \.*\* package.json yarn.lock \*.sh tailwind.config.js \*.md
